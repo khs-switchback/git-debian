5fa4e41e587d1685803ab1b70009e4cf1baba467
Mark Determan <mdeterman@gmail.com>
2014-11-14 22:55:49 +0000

    fix copy

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 if [ -d "/var/settings/ssh" ]; then
-    cp -Rs /var/settings/ssh ~/.ssh 
+    cp -r /var/settings/ssh/* ~/.ssh/
     ls ~/.ssh
 fi
-
-# Github deploy key
-#echo "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
-#echo "IdentityFile /var/settings/ssh/id_rsa" >> /root/.ssh/config
-#echo "LogLevel ERROR" >> /root/.ssh/config
