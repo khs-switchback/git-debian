ed396e3a3a386a276b12b1606de4d8ff586e410a
Mark Determan <mdeterman@gmail.com>
2014-11-20 09:19:40 -0600

    reword directory stucture

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,25 @@ RUN apt-get update && \
     apt-get install -y git && \
     rm -rf /var/lib/apt/lists/*
 
-# Setting up SSH key
-RUN mkdir -p $HOME/.ssh && mkdir -p /var/workspace && mkdir -p /var/settings
+# create all the directories that we need 
+RUN mkdir -p $HOME/.ssh 
+RUN mkdir -p /var/default 
+RUN mkdir -p /var/project
+RUN mkdir -p /var/workspace
 
+# defaults for client
+VOLUME /var/default
+# project directory
+VOLUME /var/project
+# workspace to do work
 VOLUME /var/workspace
-VOLUME /var/settings
 
+# git repository ssh url
 ENV REPO null
+# git branch name
 ENV BRANCH master
 
+# set working directory
 WORKDIR /var/workspace
 
 # add script to run
--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-if [ -d "/var/settings/ssh" ]; then
-    cat /var/settings/ssh/config >> $HOME/.ssh/config
+# setup defaults
+if [ -d "/var/default/ssh" ]; then
+	if [ -e "/var/default/ssh/config" ]; then
+		cat /var/default/ssh/config >> $HOME/.ssh/config
+	fi
+	if [ -e "/var/default/ssh/id_rsa" ]; then
+		cat /var/default/ssh/id_rsa >> $HOME/.ssh/id_rsa
+	fi
 fi
+
+# setup project
+if [ -d "/var/project/ssh" ]; then
+	if [ -e "/var/project/ssh/config" ]; then
+		rm -f $HOME/.ssh/config
+		cat /var/project/ssh/config >> $HOME/.ssh/config
+	fi
+	if [ -e "/var/project/ssh/id_rsa" ]; then
+		rm -f $HOME/.ssh/id_rsa
+		cat /var/project/ssh/id_rsa >> $HOME/.ssh/id_rsa
+	fi
+fi
+
+chmod 600 $HOME/.ssh/*
